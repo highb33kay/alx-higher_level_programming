2e93d0f2152e59614444579d8a943413002f260d
highb33kay <hiibeekayvibe@gmail.com>
2024-01-03 12:18:49 +0100

    Refactor uppercase function to use consistent indentation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for i in range(len(str)):
-		if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
-			print("{:c}".format(ord(str[i]) - 32), end="")
-		else:
-			print("{:c}".format(ord(str[i])), end="")
-	print()
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            print("{:c}".format(ord(str[i]) - 32), end="")
+        else:
+            print("{:c}".format(ord(str[i])), end="")
