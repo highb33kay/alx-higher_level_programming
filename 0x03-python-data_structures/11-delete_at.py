c44631ea648ddb4a8e2c4ba11b037054505de14f
highb33kay <hiibeekayvibe@gmail.com>
2024-01-08 18:22:28 +0100

    Refactor delete_at function and fix print statement in switch.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    # Use list slicing to create a new list without the element at the specified index
+    # Use list slicing
     new_list = my_list[:idx] + my_list[idx + 1:]
 
     return new_list
